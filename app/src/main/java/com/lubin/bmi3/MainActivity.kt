acac9822b0c63de26fac65981a34fe18b11770a1
lubin <lubinforever2020@gmal.com>
2022-03-04 17:06:40 +0800

    initial commit

--- app/src/main/java/com/lubin/bmi3/MainActivity.kt
+++ app/src/main/java/com/lubin/bmi3/MainActivity.kt
@@ -2,10 +2,25 @@ package com.lubin.bmi3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.lubin.bmi3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun bmi(view: View){
+        println("hahaha!")
+        var weight = binding.edWeight.text.toString().toFloat()
+        var height = binding.edHeight.text.toString().toFloat()
+        var bmi=weight/(height*height)
+        Log.d("BMI",bmi.toString())
     }
 }
